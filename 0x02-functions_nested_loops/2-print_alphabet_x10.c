8819f5fee19f862902cca1a2934a8fd551050abd
IsahDaniel <isahdaniel99@gmail.com>
2022-10-19 20:10:42 -0700

    Task 2 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ *followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ls;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ls = 'a'; ls <= 'z'; ls++)
+		{
+			_putchar(ls);
+		_putchar('\n');
+		}
+	}
+
+}
