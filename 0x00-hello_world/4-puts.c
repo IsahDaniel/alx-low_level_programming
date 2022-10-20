a6df66320ad67e2099a5087782fd75bc0b6d0a42
IsahDaniel <isahdaniel99@gmail.com>
2022-10-20 10:18:05 -0700

    Task 4 of 0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,11 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * 
- * Return: 0 if exited properly, non-zero otherwise
+ *
+ * Return: Always 0 (Success)
  */
-
- int main(void)
- {
- 	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- }
-
+}
