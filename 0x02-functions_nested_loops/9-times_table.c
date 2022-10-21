e7ed2740972344509c35278ea26f92be5688c763
IsahDaniel <isahdaniel99@gmail.com>
2022-10-21 07:42:05 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
  * times_table - a function that prints the 9 times table,
  *  starting with 0.
  */
-void times_table(void);
+void times_table(void)
 {
 	int i, j, k;
 
@@ -12,7 +12,7 @@ void times_table(void);
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = J * i;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -24,7 +24,8 @@ void times_table(void);
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}else if (k >= 10)
+			}
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -32,6 +33,6 @@ void times_table(void);
 				_putchar(k % 10 + '0');
 			}
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
